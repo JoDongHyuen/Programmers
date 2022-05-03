9f9d5062f4d7e512bc9fea61d4f45f0e072ae113
조동현 <32729276+JoDongHyuen@users.noreply.github.com>
2022-05-03 23:05:39 +0900

    [카카오] 오픈채팅방
    Algorithm : 구현, 문자열, 해쉬
    파이썬 써서 편하게 푼 듯

---
+++ 오픈채팅방.py
@@ -0,0 +1,24 @@
+def solution(record):
+    answer = []
+    
+    dict = {}
+    
+    for message in record:
+        words = message.split()
+        
+        if words[0] == "Enter":
+            dict[words[1]] = words[2]
+        elif words[0] == "Change":
+            dict[words[1]] = words[2]
+    
+    for message in record:
+        words = message.split()
+        
+        if words[0] == "Enter":
+            answer.append(dict[words[1]] + "님이 들어왔습니다.")
+        elif words[0] == "Leave":
+            answer.append(dict[words[1]] + "님이 나갔습니다.")
+            
+    return answer
+
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
